dc1dd6ace341c5738018eeddb6e4c064f6c587e7
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-07-22 14:47:44 +0530

    Week 06

---
+++ 06_Week_06/RDS/RDS-Multi-AZ-Demo/src/rds/multiaz/demo/ApplicationConstants.java
@@ -0,0 +1,20 @@
+package rds.multiaz.demo;
+
+public class ApplicationConstants {
+
+	public static String DRIVER_CLASS_NAME 
+		= "com.mysql.cj.jdbc.Driver";
+
+	public static String HOST = 
+	"rds-mysql-multi-az-instance-01.ctfgiek0mosx.us-east-1.rds.amazonaws.com";
+	
+	public static String DB_NAME = "mydb";
+	
+	public static String USERNAME = "admin";
+	
+	public static String PASSWORD = "password";
+	
+	public static String CONNECTION_URL
+		=	"jdbc:mysql://" + HOST + ":3306/" + DB_NAME;
+	
+}
